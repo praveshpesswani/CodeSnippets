19f2afe5f2f5284f499195f211e444eeb0e86e73
Pesswani Pravesh <PesswaniPravesh@JohnDeere.com>
2021-06-28 15:15:24 +0530

    Add project files.

---
+++ ArraySorting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeSnippets
+{
+    public class ArraySorting
+    {
+
+        public int[] SortedArray(int[] arraysOfZeroAndOne)
+        {
+
+            for(int i=0; i<arraysOfZeroAndOne.Length; i++)
+            {
+                for(int j=i+1; j< arraysOfZeroAndOne.Length; j++)
+                {
+                    if(arraysOfZeroAndOne[i] > arraysOfZeroAndOne[j])
+                    {
+                        int valueToShiftRight = arraysOfZeroAndOne[i];
+                        arraysOfZeroAndOne[i] = arraysOfZeroAndOne[j];
+                        arraysOfZeroAndOne[j] = valueToShiftRight;
+                        break;
+                    }
+                }
+            }
+
+
+            return arraysOfZeroAndOne;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraysOfZeroAndOne = { 1, 1, 1, 0, 1, 0, 0, 0 };
+            ArraySorting arraySorting = new ArraySorting();
+            int[] sortedArray = arraySorting.SortedArray(arraysOfZeroAndOne);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
